85227cc524d4dda6a4dcd2b60ce8769d497d7c58
Manas Kumar Mishra <manaskumarmishraofficial@gmail.com>
2026-09-18 00:06:26 +0530

    Refactor ApiClient by removing unnecessary Retrofit setup
    Removed redundant Retrofit builder lines for cleaner code.

--- android/app/src/main/java/com/paisaflow/app/data/network/ApiClient.kt
+++ android/app/src/main/java/com/paisaflow/app/data/network/ApiClient.kt
@@ -37,10 +37,7 @@ object ApiClient {
         .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC })
         .build()
 
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(BuildConfig.API_BASE_URL)
-        .client(http)
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+ 
         .build()
 
     val api: PaisaFlowApi = retrofit.create(PaisaFlowApi::class.java)
